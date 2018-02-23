e723ceacd476cd1383b4241e6d4dd5c526ee7b3c
kunamhitam <7kunamhitam@gmail.com>
2018-02-23 20:57:09 +0700

    versi 0.1.0

---
+++ Assets/Controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+	private float score;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.D)) {
+			GoRight();
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			GoLeft();
+		}
+
+	}
+	private void GoRight() {
+		transform.Translate (Vector3.right * 5f * Time.deltaTime);
+	}
+	private void GoLeft() {
+		transform.Translate (Vector3.left * 5f * Time.deltaTime);
+	}
+
+
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "Sphere(Clone)")
+		{
+			score++;
+			Destroy(col.gameObject);
+			Debug.Log(score);
+		}
+	}
+
+}
---
+++ Assets/Sampah.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sampah : MonoBehaviour {
+	public float delay = 0.1f;
+	public GameObject sampah;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("Spawn",delay,delay);
+	}
+	
+	// Update is called once per frame
+	void Spawn () {
+		Instantiate(sampah,new Vector3(Random.Range(-20,20),10,0),Quaternion.identity);
+	}
+
+}
